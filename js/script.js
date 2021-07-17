a4ab00f8783c35ed5a2bcb949eaf7c659e6c7a5b
kaurharmanpreet <79268786+kaurharmanpreet@users.noreply.github.com>
2021-07-16 22:47:14 -0700

    final project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*preloader*/
+window.addEventListener("load", () =>{
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(() =>{
+        document.querySelector(".preloader").style.display="none";
+    },4000)
+});
+
